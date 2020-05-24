fd3a1b0587f83d142f13a49be255020583bfd698
k07224115 <61529435+k07224115@users.noreply.github.com>
2020-05-24 20:53:48 +0800

    Update and rename test.py to scraping.py

--- scraping.py
+++ scraping.py
@@ -33,7 +33,7 @@ for i in url:
             link.append("https://news.google.com" + j.a["href"][1:])
             j = j.find_next(class_ = "wEwyrc AVN2gc uQIVzc Sksgp")
             publisher.append(j.text)
-result = pd.DataFrame({"Title": title, "標題": translate, "Publisher": publisher, "Link": link})
+result = pd.DataFrame({"編號":"", "撰寫":"", "搜尋日期":"", "新聞日期":"", "地區別":"", "國家":"", "主題類別":"", "發佈機構/來源":publisher, "標題翻譯":translate, "標題原文":title, "關鍵字":"", "負責人":"", "連結":link, "備註":""})
 now = datetime.datetime.now()
 result.to_csv(f"{now.year}.{now.month}.{now.day - 1}.csv", encoding = "utf_8_sig", index = False)
 
